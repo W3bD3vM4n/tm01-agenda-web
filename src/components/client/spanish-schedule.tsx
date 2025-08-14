571f67724f4beac9482f10a38be996ebd71eeaf1
Hassan Rahman <hr-awesome@hotmail.com>
2025-08-14 17:45:33 -0500

    Fix Syncfusion Schedule event colors

--- src/components/client/spanish-schedule.tsx
+++ src/components/client/spanish-schedule.tsx
@@ -1,15 +1,36 @@
 'use client';
 
 import React from "react";
-import { ScheduleComponent, Week, Inject, ViewsDirective, ViewDirective, EventSettingsModel, ResourcesDirective, ResourceDirective } from "@syncfusion/ej2-react-schedule";
+import {
+    ScheduleComponent,
+    Week,
+    Inject,
+    ViewsDirective,
+    ViewDirective,
+    EventSettingsModel,
+    EventRenderedArgs
+} from "@syncfusion/ej2-react-schedule";
 import { appData } from "@/app/datasource";
 
 export default function SpanishSchedule() {
     const eventSettings: EventSettingsModel = { dataSource: appData };
 
+    const onEventRendered = (args: EventRenderedArgs): void => {
+        // Apply the CategoryColor to the event element
+        if (args.data.CategoryColor) {
+            args.element.style.backgroundColor = args.data.CategoryColor;
+        }
+    };
+
     return (
         <>
-            <ScheduleComponent width='100%' height='550px' selectedDate={new Date(2018, 1, 15)} eventSettings={eventSettings} >
+            <ScheduleComponent
+                width='100%'
+                height='550px'
+                selectedDate={new Date(2018, 1, 15)}
+                eventSettings={eventSettings}
+                eventRendered={onEventRendered}
+            >
                 <ViewsDirective>
                     <ViewDirective option='Week' />
                 </ViewsDirective>
